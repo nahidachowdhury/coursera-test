ea3c9e815e837dc3a3f24ac615c9badf53876781
Nahida Chowdhury <85363526+nahidachowdhury@users.noreply.github.com>
2021-08-25 00:48:39 +0600

    Add files via upload

---
+++ Excercise.js
@@ -0,0 +1,30 @@
+//Reverse a number:
+/*function reverse_a_number(n) {
+  n = n + ``;
+  return n.split(``).reverse().join(``);
+}
+console.log(reverse_a_number());*/
+
+/*function check() {
+  var word = prompt("whats your number", "word");
+  var x = word.length;
+  var array1 = word.split("");
+  var array2 = [];
+  for (var i = (x - 1); i >= 0; i--) {
+    (array2.push(array1[i]));
+  }
+  var largo = array1.length;
+  if (array1.join("") === array2.join("")) {
+    alert("its a palindrom number");
+  }
+
+  else { alert("not a palindrom number"); }
+
+}
+alert(check()); */
+
+
+
+
+
+
